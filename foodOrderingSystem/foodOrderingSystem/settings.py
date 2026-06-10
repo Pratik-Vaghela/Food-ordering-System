75a019a237314e253b6c5de7fe84dd9cd145eddc
Pratik-Vaghela <pv1852005@gmail.com>
2026-06-10 17:32:25 +0530

    added

--- foodOrderingSystem/foodOrderingSystem/settings.py
+++ foodOrderingSystem/foodOrderingSystem/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-1bd7h=fx9-j+z@)0f2w$)vqsqu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True').lower() == 'true'
 
-ALLOWED_HOSTS = [host.strip() for host in os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',') if host.strip()]
+ALLOWED_HOSTS = [host.strip() for host in os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,.onrender.com').split(',') if host.strip()]
 
 
 # Application definition
